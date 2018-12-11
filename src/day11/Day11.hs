151e6007d8f34735f3cc45dcd7032cc7eddffb39
aplainzetakind <thisisnot@valid.org>
2018-12-11 20:07:56 +0100

    day11

--- src/day11/Day11.hs
+++ src/day11/Day11.hs
@@ -15,9 +15,9 @@ cellValue s (i, j) = subtract 5 . (`mod` 10) . (`quot` 100)
                      . (* rackID) . (+ s) . (* j) $ rackID
                         where rackID = i + 10
 
-subRow s (i, j) = sum [ cellValue s (x, j) | x <- [i..size] ]
+subRow s (i, j) = foldl' (+) 0 [ cellValue s (x, j) | x <- [i..size] ]
 
-subMatrix s (i, j) = sum [ subRow s (i, y) | y <- [j..size] ]
+subMatrix s (i, j) = foldl' (+) 0 [ subRow s (i, y) | y <- [j..size] ]
 
 memos :: M.Map (Int, Int) Int
 memos = M.fromList [ ((i, j), subMatrix serial (i, j)) | i <- [0..size]
@@ -30,8 +30,8 @@ nByN (i, j) n = lookup' (i, j) memos         - lookup' (i + n, j) memos
 
 largestSquare s = maximumBy (comparing snd)
           [ ((i, j, n), nByN (i, j) n) | i <- [0..size - 1]
-                                         , j <- [0..size - 1]
-                                         , n <- [1..min (size - i) (size - j)] ]
+                                       , j <- [0..size - 1]
+                                       , n <- [1..min (size - i) (size - j)] ]
 
 main :: IO ()
 main = print $ largestSquare serial
